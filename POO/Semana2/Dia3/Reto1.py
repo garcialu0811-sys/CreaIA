23966a9a87111e0901b7b94cea9e08a83b172243
garcialu0811-sys <garcialu0811@gmail.com>
2026-04-08 11:20:48 -0600

    actualizar

--- POO/Semana2/Dia3/Reto1.py
+++ POO/Semana2/Dia3/Reto1.py
@@ -5,4 +5,9 @@ class CuentaBancaria:
     def __init__(self, nombre_titular):
         self.nombre = nombre_titular
         self.__titular = nombre_titular
-        
+        self.__saldo = 0
+        CuentaBancaria.total_cuentas_creadas += 1
+
+    @property
+    def saldo(self):
+        pass
