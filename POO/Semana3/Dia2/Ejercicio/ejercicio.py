d93289e1b8eff6af79536c6c894fb3da27562494
garcialu0811-sys <garcialu0811@gmail.com>
2026-04-14 10:59:41 -0600

    ok

---
+++ POO/Semana3/Dia2/Ejercicio/ejercicio.py
@@ -0,0 +1,10 @@
+# Usando Forma 1:
+import random
+# Usando Forma 2:
+from random import choice
+# Usando Forma 3:
+import math as matematicas
+
+numero = random.randint(1,10)
+redondeo = matematicas.ceil(4.2)
+
