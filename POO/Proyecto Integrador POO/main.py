02bbebd6a855efd7828d457d38b0b5090f92b880
garcialu0811-sys <garcialu0811@gmail.com>
2026-04-15 11:08:11 -0600

    nuevo

--- POO/Proyecto Integrador POO/main.py
+++ POO/Proyecto Integrador POO/main.py
@@ -1,5 +1,6 @@
-# Definimos la clase POKEMON 
-class Pokemon:
+from abc import ABC, abstractmethod
+
+class Pokemon(ABC):
     def __init__(self, nombre, hp_maximo, energia_maxima):
         self.nombre = nombre
         self.hp_maximo = hp_maximo
@@ -23,26 +24,7 @@ class Pokemon:
     def energia_actual(self):
         pass
 
+    @abstractmethod
+    def atacar(self):
+        pass
 
-print("="*60)
-print(f'{"SIMULADOR DE BATALLAS POKÉMON (POO)":^60}')
-print("="*60)
-print("Seleccione el Modo de Juego:")
-print("1. Jugador vs Jugador")
-print("2. Jugador vs Computadora")
-opcion = int(input("> Opción: "))
-
-while True:
-    print("="*60)
-    print(f'{"CATALOGO POKÉMON OFICIAL":^60}')
-    print("="*60)
-    
-
-    match opcion:
-        case "1":
-            pass
-        case "2":
-            pass
-        case _:
-            print("Opcion invalida")
-    print("Seleccione el Modo de Juego:")
